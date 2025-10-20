0b66c0bb5c4cded50bd06c6455c5e094384271a5
pantherinblack <stupark@bzz.ch>
2025-10-20 09:13:06 +0200

    feat: frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Box, Button, Typography} from '@mui/material';
+
+export function App() {
+  return (
+    <div className="App">
+      <header>
+        <Typography variant="h1">
+          Ai-Poet Generation
+        </Typography>
+      </header>
+    </div>
+  );
+}
