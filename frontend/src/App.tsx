cdfc62345f07c0ee57cf1f4509b95a4a54787d0a
pantherinblack <stupark@bzz.ch>
2025-10-20 09:16:22 +0200

    feat: prettier setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import {Box, Button, Typography} from '@mui/material';
+import React from "react";
+import { Typography } from "@mui/material";
 
 export function App() {
   return (
     <div className="App">
       <header>
-        <Typography variant="h1">
-          Ai-Poet Generation
-        </Typography>
+        <Typography variant="h1">Ai-Poet Generation</Typography>
       </header>
     </div>
   );
